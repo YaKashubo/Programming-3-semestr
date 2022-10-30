5880719cf317297530da9dd93a31c13d9ae89ccb
YaKashubo <90766415+YaKashubo@users.noreply.github.com>
2022-10-31 00:38:56 +0300

    2nd commit
    2nd commit

--- 2022.11.01-Homework6/2022.11.01-Homework6/Source.cpp
+++ 2022.11.01-Homework6/2022.11.01-Homework6/Source.cpp
@@ -8,33 +8,59 @@ int main()
 {
 	ifstream f("data.txt");
 	int n = 0;
-	f >> n;
-
-	int* a = new int[n];
-	for (int i = 0; i < n; ++i)
+	try
 	{
-		f >> a[i];
+		if (!(f.is_open()))	throw "file didn't open.";
+		else
+		{
+			f >> n;
+			if (n <= 0 || (typeid(n) != typeid(int)))
+			{
+				throw "wrong array's range.";
+			}
+			int* a = new int[n];
+			int realn = 0;
+			for (int i = 0; i < n; ++i)
+			{
+				string s;
+				f >> s;
+				if (!(s.find_first_not_of("-0123456789") == string::npos)) throw "wrong type of element.";
+				a[i] = stoi(s);
+				realn++;
+			}
+			if (realn == 0) throw "empty array.";
+			else
+			{
+				double ratio = 0;
+				double geomMean = 0.0;
+				int sum = 0;
+				for (int i = 0; i < realn - 1; ++i)
+				{
+					if (long long(sum + a[i]) > pow(2, 8 * sizeof(int))) throw "type overflow.";
+					else sum += a[i];
+					if (realn == 1) throw "out of array's range.";
+					else
+					{
+						if (a[i + 1] == 0) throw "dividing by 0.";
+						ratio += (a[i] / a[i + 1]);
+					}
+					if (a[i] <= 0) throw "math logic error.";
+					else geomMean += log(a[i]);
+				}
+				sum += a[realn - 1];
+				geomMean += log(a[realn - 1]);
+				ratio /= realn;
+				geomMean = exp((geomMean / realn));
+				cout << "Summary of array is " << sum << endl;
+				cout << "Geometric mean of array is " << geomMean << endl;
+				cout << "Avarage ratio of array's element is " << ratio << endl;
+			}
+		}
 	}
-
-	double ratio = 0;
-	double geomMean = 0.0;
-	int sum = 0;
-	for (int i = 0; i < n - 1; ++i)
+	catch (const char* msg)
 	{
-		sum += a[i];
-		ratio += (a[i] / a[i + 1]);
-		geomMean += log(a[i]);
+		cerr << "Error: " << msg << endl;
 	}
-	sum += a[n - 1];
-	geomMean += log(a[n - 1]);
-
-	ratio /= n;
-	geomMean = exp((geomMean / n));
-
-	cout << "Summary of array is " << sum << endl;
-	cout << "Geometric mean of array is " << geomMean << endl;
-	cout << "Avarage ratio of array's element is " << ratio << endl;
-
 	f.close();
 	return EXIT_SUCCESS;
 }
